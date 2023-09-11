54d58e55d97c8265b1c4e69f231c331600284a67
João Monteiro <monteiro.joao.ps@gmail.com>
2023-09-11 19:55:23 +0000

    precommit and mypy

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,5 +4,8 @@ poetry install --with dev,docs
 # Activate environment
 sh ./.venv/bin/activate
 
+# Install pre-commit hooks
+pre-commit install-hooks
+
 # Login into GCP
 gcloud auth application-default login
