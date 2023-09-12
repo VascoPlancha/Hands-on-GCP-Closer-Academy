2ed81be46b22ba631a349357cfef08c26dd9657a
João Monteiro <monteiro.joao.ps@gmail.com>
2023-09-12 12:09:29 +0000

    fix module paths

--- functions/simple_mlops/ingest_data/app/__init__.py
+++ functions/simple_mlops/ingest_data/app/__init__.py
@@ -1,3 +1,3 @@
 """Ingest Data App."""
-from . import _models as id_models  # noqa
-from . import _transform as id_transform  # noqa
+from ._modules import _models as id_models  # noqa
+from ._modules import _transform as id_transform  # noqa
